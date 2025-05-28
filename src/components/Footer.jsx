0508a416c5d9c6b7667f827e5f643de3f964ef78
durga <durgamadhabnayak07@gmail.com>
2025-05-28 13:58:38 +0530

    done1

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,10 +11,10 @@ function Footer() {
           <div className="wave" id="wave4"></div>
         </div>
         <ul className="social_icon ">
-          <li><a href=""><ion-icon name="logo-github"></ion-icon></a></li>
-          <li><a href=""><ion-icon name="logo-twitter"></ion-icon></a></li>
-          <li><a href=""><ion-icon name="logo-linkedin"></ion-icon></a></li>
-          <li><a href=""><ion-icon name="logo-instagram"></ion-icon> </a></li>
+          <li><a href="https://github.com/slaorvaeda"><ion-icon name="logo-github"></ion-icon></a></li>
+          <li><a href="https://x.com/KanhuNayak980"><ion-icon name="logo-twitter"></ion-icon></a></li>
+          <li><a href="https://www.linkedin.com/in/india-durganayak/"><ion-icon name="logo-linkedin"></ion-icon></a></li>
+          <li><a href="https://www.instagram.com/priyanshu.ind/"><ion-icon name="logo-instagram"></ion-icon> </a></li>
         </ul>
         <div className="logo "><dir></dir>dURGA</div>
       </footer>
